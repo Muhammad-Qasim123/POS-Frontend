9a7bbd714db4d8a3600694b7d444edf61600c7d2
Qasim_gitHub <qasimab222@gmail.com>
2023-09-25 07:14:58 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import ItemPage from "./pages/itemPage";
-
+import CartPage from "./pages/cartPage";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/items" element={<ItemPage />} />
+        <Route path="/cart" element={<CartPage/>}/>
       </Routes>
     </div>
   );
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SideBar from '../components/defaultLayout';
+import axios from 'axios';
+import { Row, Col } from 'antd';
+import ItemList from '../components/itemList';
 
 const HomePage = () => {
+    const [itemsData, setItemsData] = useState([])
+    useEffect(() => {
+        const getAllItems = async () => {
+            try {
+                const { data } = await axios.get('/api/items/get-item')
+                setItemsData(data);
+                console.log(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getAllItems()
+    }, [])
     return (
         <div>
-            <SideBar><h1>Hello Giii</h1></SideBar>
-        
+            <SideBar>
+                <Row>
+                    {
+                        itemsData.map(item => (
+                            <Col xs={24} lg={6} md={12} sm={6}>
+                                <ItemList item={item} />
+                            </Col>
+                        ))
+                    }
+                </Row>
+            </SideBar>
+
         </div>
     );
 };
